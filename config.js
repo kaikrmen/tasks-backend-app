619ba2018a82e846a8a69c3d3dddf1795f7cda2a
Carmen Leon <61640662+kaikrmen@users.noreply.github.com>
2024-12-29 12:29:58 -0400

    add proccess env

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const MONGODB_URI = "mongodb://localhost/tasks-app";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/tasks-app";
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
